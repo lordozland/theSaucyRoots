ae6e99114e858e5fccf0b4ef21ec05dc1a5cbba8
William Crews <crewswh@gmail.com>
2021-08-19 15:06:50 -0400

    truth-ish

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,27 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_kewl: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    driver_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'driver',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
